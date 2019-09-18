f3e6c4db97f6bb6cb335cd060044e2570bb6f536
Quinn Ellis <quinn@packup.io>
2019-09-18 07:56:26 -0700

    Set up feed and root storyboards/initial vc's

---
+++ Voyce/Feed/FeedViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewController.swift
+//  Voyce
+//
+//  Created by Quinn Ellis on 9/18/19.
+//  Copyright © 2019 QEDev. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+  
+}
---
+++ Voyce/Supporting Files/Color.swift
@@ -0,0 +1,27 @@
+//
+//  Color.swift
+//  Voyce
+//
+//  Created by Quinn Ellis on 9/18/19.
+//  Copyright © 2019 QEDev. All rights reserved.
+//
+
+import UIKit
+
+public enum Color: String {
+
+  case BackgroundDarkBlue
+
+  public var uiColor: UIColor {
+    return UIColor(named: self.rawValue)!
+  }
+
+  public var cgColor: CGColor {
+    return  UIColor(named: self.rawValue)!.cgColor
+  }
+
+  public func withAlpha(_ component: CGFloat) -> UIColor {
+    return self.uiColor.withAlphaComponent(component)
+  }
+
+}
