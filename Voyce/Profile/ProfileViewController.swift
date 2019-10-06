61480678b8942e9a58c80cb2907b5b0c7dd1f063
Pedro Leiva <pedrolei@usc.edu>
2019-10-06 12:02:52 -0700

    Profile loads in username on load

--- Voyce/Profile/ProfileViewController.swift
+++ Voyce/Profile/ProfileViewController.swift
@@ -9,6 +9,18 @@
 import Foundation
 import UIKit
 
+private let userManager = UserManager.shared
+
 class ProfileViewController: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
     
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var goodVibesLabel: UILabel!
+    
+    override func viewDidLoad() {
+        usernameLabel.text = "@" + userManager.username
+    }
+    @IBAction func transferToBankPressed(_ sender: Any) {
+//        add functionality for transferring Good Vibes to bank
+    }
 }
