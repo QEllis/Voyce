ea16c2946f50c44f29b853d50cae59377e5ad3a8
Nathan Gifford <ngifford@usc.edu>
2019-10-22 14:18:34 -0700

    more ad work

--- Voyce/Ad/AdViewController.swift
+++ Voyce/Ad/AdViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class AdViewController: UIViewController {
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
+        initTimer()
     }
     
+    var vibez = 0 //init to user data
+    var adNumber = 0
+    var timer = Timer()
+    
     @IBOutlet var vibezLabel: UILabel!
+    @IBOutlet var adLabel: UILabel!
     
     @IBAction func handleAdSwipe(recognizer:UISwipeGestureRecognizer) {
         let translation = recognizer.location(in: self.view)
@@ -21,6 +27,18 @@ class AdViewController: UIViewController {
             view.center = CGPoint(x:view.center.x + translation.x,
                                   y:view.center.y + translation.y)
         }
+        adNumber += 1
+        adLabel.text = "ad number \(adNumber)"
+    }
+    
+    func initTimer(){
+        // Scheduling timer to Call the function "updateCounting" with the interval of 1 seconds
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateViewTime), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateViewTime(){
+        vibez += 1
+        vibezLabel.text = "Good Vibez: \(vibez)"
     }
 
 }
