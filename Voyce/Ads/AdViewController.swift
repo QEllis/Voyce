e97e0d98e7b8daad1276ec3e2ba03dccae3505e5
Pedro Leiva <pedrolei@usc.edu>
2019-11-03 15:38:02 -0800

    Trying to fix pod deletion

--- Voyce/Ads/AdViewController.swift
+++ Voyce/Ads/AdViewController.swift
@@ -9,58 +9,79 @@
 import UIKit
 
 class AdViewController: UIViewController {
+  
+  var vibes = 0
+  var adNumber = 0
+  var timer = Timer()
+  var ads = NSMutableArray()
+  var icon = UIImage(named: "wand")
+  
+  override func viewDidLoad() {
+    print("HERE")
+    super.viewDidLoad();
+    initTimer()
+    ads.addObjects(from: ["this is our first test ad", icon!, "this is our second test ad", "this is our third test ad"]) //FRANK assuming this will come in as an array of Strings or UIImages
+    vibes = 0
     
-    var vibez = 0
-    var adNumber = 0
-    var timer = Timer()
-    var ads = NSMutableArray()
-    var showingTextAd : Bool = true
-    
-    override func viewDidLoad() {
-        super.viewDidLoad();
-        initTimer()
-        ads.addObjects(from: ["this is our fist test ad", "this is our second test ad", "this is our third test ad"]) //FRANK assuming this will come in as an array of Strings or UIImages
-        if ads[0] is String {
-            showingTextAd = true
-        } else {
-            showingTextAd = false
-        }
-        vibez = 0
-    }
-    
-    @IBOutlet var vibezLabel: UILabel!
-    @IBOutlet var adLabel: UILabel!
-    @IBOutlet var imageAdView: UIImageView!
-    
-    @IBAction func handleAdSwipe(recognizer:UISwipeGestureRecognizer) {
-        let translation = recognizer.location(in: self.view)
-        if let view = recognizer.view {
-            view.center = CGPoint(x:view.center.x + translation.x,
-                                  y:view.center.y + translation.y)
-        }
+    let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(swipeHandler(sender:)))
+    rightSwipe.direction = .right
+    view.addGestureRecognizer(rightSwipe)
+    let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(swipeHandler(sender:)))
+    leftSwipe.direction = .left
+    view.addGestureRecognizer(rightSwipe)
+    view.addGestureRecognizer(leftSwipe)
+  }
+  
+  @IBOutlet var vibezLabel: UILabel!
+  @IBOutlet var adLabel: UILabel!
+  @IBOutlet var videoLabel: UILabel!
+  @IBOutlet var imageAdView: UIImageView!
+  
+  
+  @IBAction func swipeHandler(sender : UISwipeGestureRecognizer) {
+    if sender.state == .ended {
+      // Perform action.
+      switch sender.direction {
+      case .right :
         adNumber += 1
-        if adNumber >= ads.count {
-            adNumber = 0
-        }
-        if ads[adNumber] is String {
-            showingTextAd = true
-            adLabel.text = ads[adNumber] as? String
-        } else {
-            showingTextAd = false
-            imageAdView.image = ads[adNumber] as? UIImage
-        }
-        adLabel.isHidden = !showingTextAd
-        imageAdView.isHidden = showingTextAd
+      case .left:
+        adNumber -= 1
+      default:
+        break
+      }
+      if adNumber >= ads.count {
+        adNumber = 0
+      }
+      if adNumber < 0 {
+        adNumber = ads.count-1
+      }
+      print(adNumber)
+      if ads[adNumber] is String {
+        adLabel.text = ads[adNumber] as? String
+        adLabel.isHidden = false
+        imageAdView.isHidden = true
+        videoLabel.isHidden = true
+      } else if ads[adNumber] is UIImage {
+        imageAdView.image = ads[adNumber] as? UIImage
+        adLabel.isHidden = true
+        imageAdView.isHidden = false
+        videoLabel.isHidden = true
+      } else {
+        adLabel.isHidden = true
+        imageAdView.isHidden = true
+        videoLabel.isHidden = false
+      }
     }
-    
-    func initTimer(){
-        // Scheduling timer to Call the function "updateViewTime" with the interval of 1 seconds
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateViewTime), userInfo: nil, repeats: true)
-    }
-    
-    @objc func updateViewTime(){
-        vibez += 1 //FRANK update server side amount of vibez
-        vibezLabel.text = "Good Vibez: \(vibez)"
-    }
-
+  }
+  
+  func initTimer(){
+    // Scheduling timer to Call the function "updateViewTime" with the interval of 1 seconds
+    timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateViewTime), userInfo: nil, repeats: true)
+  }
+  
+  @objc func updateViewTime(){
+    vibes += 1 //FRANK update server side amount of vibez
+    vibezLabel.text = "Good Vibes: \(vibes)"
+  }
+  
 }
