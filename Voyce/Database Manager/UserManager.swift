ce4c381023beaee5f59e33fac95e56b63f3db6b0
Aron Vischjager <vischjag@usc.edu>
2020-03-06 18:16:47 -0800

    Update to folder name

---
+++ Voyce/Database Manager/UserManager.swift
@@ -0,0 +1,248 @@
+//
+//  UserManager.swift
+//  Voyce
+//
+//  Created by Quinn Ellis on 9/19/19.
+//  Copyright © 2019 QEDev. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
+
+class databaseManager
+{
+    // Member Variables
+    static let shared = databaseManager()
+    var sharedUser: User
+    var db: Firestore
+    var storage: Storage
+    var dislikedAds: [String]
+    var acknowledgedPosts:[String:Post]
+    var myPosts: Array<Post>
+    var posts: Array<Post> = []
+    {
+        didSet
+        {
+            NotificationCenter.default.post(name: .NewPosts, object: nil)
+        }
+    }
+    // User Manager Construcor
+    init()
+    {
+        // Default user that gets over written when user logs in
+        sharedUser = User.init(userID: "0", name: "null", username: "null")
+        db = Firestore.firestore()
+        storage = Storage.storage()
+        dislikedAds = []
+        acknowledgedPosts = [:]
+        myPosts = []
+    }
+    
+    // Member Functions
+    public func checkAcknowledgedPost(post:Post)->Bool
+    {
+        if(acknowledgedPosts[post.postID] != nil)
+        {
+            return true
+        }
+        else
+        {
+            return false
+        }
+    }
+
+    public func updateVibes(post:Post)
+    {
+        print("I AM HERE");
+        post.likeCount+=1;
+        print(post.postID);
+        db.collection("posts").document(post.postID).setData([ "likeCount": post.likeCount ], merge: true);
+    }
+
+    /*
+    public func UnacknowledgedPost(post:Post) {
+    if(checkAcknowledgedPost(post: post)) {
+      print("Deacknowledged Post")
+      acknowledgedPosts.removeValue(forKey: post.postID)
+      post.likeCount-=1;
+    }
+    }
+    */
+
+    public func userLogin(u: FirebaseAuth.User) {
+    sharedUser  = User.init(user: u)
+    let collection = db.collection("users");
+    let userDoc = collection.document(u.uid)
+
+    userDoc.getDocument { (document, error) in
+      if let document = document, document.exists {
+          self.sharedUser.LoadUserData(dataDict: document.data()!)
+      } else {
+          print("User does not exist yet, create in db", u.uid)
+          userDoc.setData(self.sharedUser.dictionary)
+      }
+    }
+    }
+
+    public func createHardcodedPosts() {
+    let post1 = Post(pid: "1", text: "Just had a great time at the getty", media: "",
+                     user: User(userID: "19", name: "Quinn", username: "quinn", goodVibes: 19), likeCount: 3, image: UIImage(named: "Getty"))
+    post1.addComment(Post(pid: "5", text: "Pedro: Nice!", media: "", user: User(userID: "1", name: "Pedro", username: "pedro", goodVibes: 5), likeCount: 1))
+    let myPost = Post(pid: "1", text: "Promotion!", media: "",
+                      user: User(userID: "19", name: "Quinn", username: "quinn", goodVibes: 19), likeCount: 3)
+    myPosts = [myPost]
+    let post2 = Post(pid: "2", text: "What's your favorite type of food?", media: "", user: User(userID: "1", name: "Pedro", username: "pedro", goodVibes: 5), likeCount: 1)
+    post2.addComment(Post(pid: "5", text: "Quinn: Pizza for sure", media: "", user: User(userID: "19", name: "Quinn", username: "quinn", goodVibes: 5), likeCount: 0))
+    let post3 = Post(pid: "3", text: "Concert time!", media: "", user: User(userID: "2", name: "Frank", username: "frank", goodVibes: 10), likeCount: 0, image: UIImage(named: "Concert"))
+    let post4 = Post(pid: "4", text: "So nice out today.", media: "", user: User(userID: "3", name: "Cole", username: "cole", goodVibes: 25), likeCount: 3, image: UIImage(named: "Beach"))
+    posts = [post1, post2, post3, post4]
+    }
+
+    public func addPost(with text: String, image: UIImage){
+    let id = UUID()
+    let newPost = Post(pid: "0", text: text, media: "", user: sharedUser, likeCount: 0, image: image)
+    myPosts.insert(newPost, at: 0)
+    posts.insert(newPost, at: 0)
+    db.collection("posts").document(id.uuidString).setData([
+        "uid": sharedUser.userID,
+        "ts": NSDate().timeIntervalSince1970,
+        "text": text,
+        "media": "",
+        "likeCount": 0,
+        "image": ""
+    ]) { err in
+        if let err = err {
+            print("Error writing post to db: \(err)")
+        } else {
+            print("image post successfully written to db!")
+        }
+    }
+
+    }
+
+
+    public func addPost(with text: String) {
+    let id = UUID()
+    let newPost = Post(pid: "-1", text: text, media: "", user: sharedUser, likeCount: 0)
+    myPosts.insert(newPost, at: 0)
+    posts.insert(newPost, at: 0)
+        
+    db.collection("posts").document(id.uuidString).setData([
+        "uid": sharedUser.userID,
+        "ts": NSDate().timeIntervalSince1970,
+        "text": text,
+        "media": "",
+        "likeCount": 0,
+        "postID": id.uuidString
+    ]) { err in
+        if let err = err {
+            print("Error writing post to db: \(err)")
+        } else {
+            print("post successfully written to db!")
+        }
+    }
+    }
+
+    public func SetPostsUser(uid: String, p:Post){
+        
+        let collection = db.collection("users")
+        collection.document(uid)
+            .getDocument() { (document, error) in
+                if let document = document, document.exists {
+                    let data = document.data()
+                    let dataDescription = data.map(String.init(describing:)) ?? "nil"
+                    print("GET USER Doc id: \(document.documentID)")
+                    print("GET USER Document data: \(dataDescription)")
+                    let actualUserFound = User(userID: document.documentID,
+                                        name:  document.get("name") as! String,
+                                        username:  document.get("username") as! String,
+                                        imageURL:  document.get("imageURL") as! String,
+                                        goodVibes:  document.get("goodvibes")  as! Int)
+                    p.user = actualUserFound
+                  databaseManager.shared.posts.append(p)
+                    print("Post Creator: \(p.user.userID)")
+                } else {
+                    print("User does not exist")
+                }
+            }
+    }
+
+    public func LoadFeed(){
+        print("IN LOAD FEED");
+        let collection = db.collection("posts")
+        collection.order(by: "likeCount", descending: true).limit(to: 10)
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    self.posts = []
+                    var uids: [String] = []
+                    var newPosts: [Post] = []
+                    for document in querySnapshot!.documents {
+                        let data = document.data()
+                        let p = Post(pid: document.documentID,
+                                     text: data["text"] as! String,
+                                     media: data["media"] as! String,
+                                     user: User(),
+                                     likeCount: data["likeCount"] as! Int)
+    //                        self.SetPostsUser(uid: data["uid"] as! String, p: p)
+    //                        self.posts.append(p)
+                        uids.append(data["uid"] as! String)
+                        newPosts.append(p)
+                        print("POST: \(document.documentID) => \(document.data())")
+                    }
+                    for i in 0..<newPosts.count {
+                        self.SetPostsUser(uid: uids[i], p: newPosts[i])
+                    }
+                }
+        }
+    }
+
+    public func UpdatePosts(){
+        let collection = db.collection("posts");
+        for post in posts{
+            
+            db.collection("posts").document(post.postID).setData(post.dictionary) { err in
+                if let err = err {
+                    print("Error updating post to db: \(err)")
+                } else {
+                    print("post successfully updated to db!")
+                }
+            }
+            
+            let postDoc = collection.document(post.postID)
+            postDoc.getDocument { (document, error) in
+              if let document = document, document.exists {
+                
+              } else {
+                print("UPDATE POST: Post does not exist", post.postID)
+               }
+            }
+        }
+        
+        posts = []
+    }
+
+    public func addComment(with text: String, post: Post) {
+    for currPost in posts where currPost.postID == post.postID {
+      print("addComment")
+      let comment = Post(pid: "comment", text: text,media: "", user: databaseManager.shared.sharedUser, likeCount: 0)
+      //UserManager.shared.addComment(with: text, post: currPost)
+      currPost.addComment(comment)
+    }
+    NotificationCenter.default.post(name: .NewPosts, object: nil)
+    }
+
+    public func addFollowed(username:String){
+    databaseManager.shared.sharedUser.addFollowed(username: username)
+    }
+    public func removeFollowed(username:String){
+    databaseManager.shared.sharedUser.removeFollowed(username: username)
+    }
+
+    public func checkIfFollowed(username:String)->Bool{
+    return databaseManager.shared.sharedUser.checkIfFollowed(username: username)
+    }
+}
