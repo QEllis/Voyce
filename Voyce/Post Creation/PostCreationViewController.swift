aa0fa3f0abdd4c93dfbd2b0b17f1ecf93d23fc1b
Jordan Ghidossi <ghidossi@usc.edu>
2020-03-08 22:47:28 -0700

    Add Post UI updated

--- Voyce/Post Creation/PostCreationViewController.swift
+++ Voyce/Post Creation/PostCreationViewController.swift
@@ -121,5 +121,59 @@ extension PostCreationViewController: UIImagePickerControllerDelegate, UINavigat
             imageRef.downloadURL { (url, error) in guard let downloadURL = url else { return }
             }
         }
+        
+        
+        
+
+//        // Create the file metadata
+//        let metadata = StorageMetadata()
+//        metadata.contentType = "image/jpeg"
+//
+//        // Upload file and metadata to the object 'images/mountains.jpg'
+//        let uploadTask = storageRef.putFile(from: imageURL, metadata: metadata)
+//
+//        // Listen for state changes, errors, and completion of the upload.
+//        uploadTask.observe(.resume) { snapshot in
+//          // Upload resumed, also fires when the upload starts
+//        }
+//
+//        uploadTask.observe(.pause) { snapshot in
+//          // Upload paused
+//        }
+//
+//        uploadTask.observe(.progress) { snapshot in
+//          // Upload reported progress
+//          let percentComplete = 100.0 * Double(snapshot.progress!.completedUnitCount)
+//            / Double(snapshot.progress!.totalUnitCount)
+//        }
+//
+//        uploadTask.observe(.success) { snapshot in
+//          // Upload completed successfully
+//        }
+//
+//        uploadTask.observe(.failure) { snapshot in
+//          if let error = snapshot.error as? NSError {
+//            switch (StorageErrorCode(rawValue: error.code)!) {
+//            case .objectNotFound:
+//              // File doesn't exist
+//              break
+//            case .unauthorized:
+//              // User doesn't have permission to access file
+//              break
+//            case .cancelled:
+//              // User canceled the upload
+//              break
+//
+//            /* ... */
+//
+//            case .unknown:
+//              // Unknown error occurred, inspect the server response
+//              break
+//            default:
+//              // A separate error occurred. This is a good place to retry the upload.
+//              break
+//            }
+//          }
+//        }
     }
 }
