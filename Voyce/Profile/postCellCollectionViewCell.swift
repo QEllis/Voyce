4e979be987fee9beef3ad1ec3eb3a1ff3de17927
Matt <hbarbar@usc.edu>
2020-04-19 13:18:44 -0700

    post cell file

---
+++ Voyce/Profile/postCellCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  postCellCollectionViewCell.swift
+//  Voyce
+//
+//  Created by Hekmat on 4/19/20.
+//  Copyright © 2020 QEDev. All rights reserved.
+//
+
+import UIKit
+
+class postCellCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var postImage: UIImageView!
+    
+}
